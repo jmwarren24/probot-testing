e544b9a9291fd9a1915860e2c0b1ee0001832819
jake.warren <jake.warren@cherwell.com>
2021-12-09 16:11:29 -0600

    Updating more stuff

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const app = require("express")();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+
+app.listen(3000, () => console.log("Connected to Server"));
+
+app.get("/",(req,res) => {
+    res.send("Server is up");
+});
+
+const routes = require("./routes/routes");
+
+app.use("/routes", routes);
---
+++ api/routes/routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+router.get("/ping",(req,res) => {
+    res.json({ Response: "pong" });
+});
+
+router.post("/pong", (req,res) => {
+    const request = req.body;
+    if(request.pong) {
+        res.json(request);
+    }
+    else {
+        res.status(400).json({"Error":"Not Allowed"});
+    }
+});
+
+router.patch("/pingpong",(req,res) => {
+    const request = req.body;
+    if(request._id == "ping") {
+        res.json({"Response":"Pong"});
+    }
+    else {
+        res.status(400).json({"Error":"Not Allowed"});
+    }
+});
+
+
+module.exports = router;
